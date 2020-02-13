backend-devloper/tutanota

commit 152846866da7c6e0d849ae454d86939c034075a8

Access mailboxDetails in async way, fix #1748, fix #1112, fix #1184

src/search/SearchView.js
@@ -183,7 +183,7 @@ export class SearchView implements CurrentView {
 							: isSameTypeRef(restriction.type, MailTypeRef) && isNewMailActionAvailable()
 							? m(".mlr-l.mt", m(ButtonN, {
 								click: () => {
-									newMail(mailModel.getUserMailboxDetails()).catch(PermissionError, noOp)
+									mailModel.getUserMailboxDetails().then(newMail).catch(PermissionError, noOp)
 								},
 								label: "newMail_action",
 								type: ButtonType.PrimaryBorder,
@@ -263,7 +263,7 @@ export class SearchView implements CurrentView {
 			isSameTypeRef(restriction.type, MailTypeRef) && isNewMailActionAvailable()
 				? m(ButtonN, {
 					click: () => {
-						newMail(mailModel.getUserMailboxDetails()).catch(PermissionError, noOp)
+						mailModel.getUserMailboxDetails().then(newMail).catch(PermissionError, noOp)
 					},
 					label: "newMail_action",
 					type: ButtonType.Action,
@@ -415,7 +415,7 @@ export class SearchView implements CurrentView {
 				exec: () => {
 					const restriction = getRestriction(m.route.get()).type
 					if (isSameTypeRef(restriction, MailTypeRef)) {
-						newMail(mailModel.getUserMailboxDetails()).catch(PermissionError, noOp)
+						mailModel.getUserMailboxDetails().then(newMail).catch(PermissionError, noOp)
 					} else if (isSameTypeRef(restriction, ContactTypeRef)) {
 						LazyContactListId.getAsync().then(contactListId => {
 							new ContactEditor(null, contactListId, null).show()

src/calendar/CalendarSharingUtils.js
@@ -14,15 +14,18 @@ import {GroupMemberTypeRef} from "../api/entities/sys/GroupMember"
 
 
 export function sendShareNotificationEmail(sharedGroupInfo: GroupInfo, recipients: Array<RecipientInfo>) {
-	const senderMailAddress = getDefaultSender(mailModel.getUserMailboxDetails())
-	const replacements = {
-		// Sender is displayed like Name <mail.address@tutanota.com>. Less-than and greater-than must be encoded for HTML
-		"{inviter}": senderMailAddress,
-		"{calendarName}": getCalendarName(sharedGroupInfo, false)
-	}
-	// Sending notifications as bcc so that invited people don't see each other
-	const bcc = recipients.map(({name, mailAddress}) => ({name, address: mailAddress}))
-	_sendNotificationEmail({bcc}, "shareCalendarInvitationEmailSubject_msg", "shareCalendarInvitationEmailBody_msg", senderMailAddress, replacements)
+	mailModel.getUserMailboxDetails().then((mailboxDetails) => {
+		const senderMailAddress = getDefaultSender(mailboxDetails)
+		const replacements = {
+			// Sender is displayed like Name <mail.address@tutanota.com>. Less-than and greater-than must be encoded for HTML
+			"{inviter}": senderMailAddress,
+			"{calendarName}": getCalendarName(sharedGroupInfo, false)
+		}
+		// Sending notifications as bcc so that invited people don't see each other
+		const bcc = recipients.map(({name, mailAddress}) => ({name, address: mailAddress}))
+		_sendNotificationEmail({bcc}, "shareCalendarInvitationEmailSubject_msg", "shareCalendarInvitationEmailBody_msg", senderMailAddress,
+			replacements)
+	})
 }
 
 
@@ -49,13 +52,19 @@ export function sendRejectNotificationEmail(invitation: ReceivedGroupInvitation)
 }
 
 
-function _sendNotificationEmail(recipients: Recipients, subject: TranslationKey, body: TranslationKey, senderMailAddress: string, replacements: {[string]: string}) {
-	const editor = new MailEditor(mailModel.getUserMailboxDetails())
-	const sender = getEnabledMailAddresses(mailModel.getUserMailboxDetails()).includes(senderMailAddress) ? senderMailAddress : getDefaultSender(mailModel.getUserMailboxDetails())
-	const subjectString = lang.get(subject)
-	const bodyString = lang.get(body, replacements)
-	editor.initWithTemplate(recipients, subjectString, bodyString, true, sender)
-	editor.send(false, "tooManyMailsAuto_msg")
+function _sendNotificationEmail(recipients: Recipients, subject: TranslationKey, body: TranslationKey, senderMailAddress: string,
+                                replacements: {[string]: string}) {
+	mailModel.getUserMailboxDetails().then((mailboxDetails) => {
+		const editor = new MailEditor(mailboxDetails)
+		const sender = getEnabledMailAddresses(mailboxDetails).includes(senderMailAddress)
+			? senderMailAddress
+			: getDefaultSender(mailboxDetails)
+		const subjectString = lang.get(subject)
+		const bodyString = lang.get(body, replacements)
+		editor.initWithTemplate(recipients, subjectString, bodyString, true, sender)
+		editor.send(false, "tooManyMailsAuto_msg")
+	})
+
 }
 
 export type GroupMemberInfo = {

src/contacts/ContactViewer.js
@@ -205,13 +205,15 @@ export class ContactViewer {
 	}
 
 
-	_writeMail(mailAddress: string) {
-		let editor = new MailEditor(mailModel.getUserMailboxDetails())
-		const name = `${this.contact.firstName} ${this.contact.lastName}`.trim()
-		editor.initWithTemplate({to: [{name, address: mailAddress}]}, "", getEmailSignature(), null)
-		      .then(() => {
-			      editor.show()
-		      })
+	_writeMail(mailAddress: string): Promise<*> {
+		return mailModel.getUserMailboxDetails().then((mailboxDetails) => {
+			const editor = new MailEditor(mailboxDetails)
+			const name = `${this.contact.firstName} ${this.contact.lastName}`.trim()
+			return editor.initWithTemplate({to: [{name, address: mailAddress}]}, "", getEmailSignature(), null)
+			             .then(() => {
+				             editor.show()
+			             })
+		})
 	}
 
 	_setupShortcuts() {

src/gui/base/Button.js
@@ -283,12 +283,23 @@ export function createAsyncDropDownButton(labelTextIdOrTextFunction: string | la
 		}
 		let buttonPromise = lazyButtons()
 		let resultPromise = buttonPromise
-		if (!resultPromise.isFulfilled()) {
-			resultPromise = asyncImport(typeof module !== "undefined" ? module.id : __moduleName,
-				`${env.rootPathPrefix}src/gui/base/ProgressDialog.js`)
-				.then(module => {
-					return module.showProgressDialog("loading_msg", buttonPromise)
-				})
+		// If the promise is pending and does not resolve in 100ms, show progress dialog
+		if (buttonPromise.isPending()) {
+			resultPromise = Promise.race([
+					buttonPromise,
+					Promise.all([
+						Promise.delay(100),
+						asyncImport(typeof module !== "undefined" ? module.id : __moduleName,
+							`${env.rootPathPrefix}src/gui/base/ProgressDialog.js`)
+					]).then(([_, module]) => {
+						if (buttonPromise.isPending()) {
+							return module.showProgressDialog("loading_msg", buttonPromise)
+						} else {
+							return buttonPromise
+						}
+					})
+				]
+			)
 		}
 		const initialButtonRect: PosRect = mainButton._domButton.getBoundingClientRect()
 		resultPromise.then(buttons => {

src/gui/base/DropdownN.js
@@ -325,12 +325,23 @@ export function createAsyncDropdown(lazyButtons: lazyAsync<$ReadOnlyArray<DropDo
 	return ((e, dom) => {
 		let originalButtons = lazyButtons()
 		let buttons = originalButtons
-		if (!buttons.isFulfilled()) {
-			buttons = asyncImport(typeof module !== "undefined" ? module.id : __moduleName,
-				`${env.rootPathPrefix}src/gui/base/ProgressDialog.js`)
-				.then(module => {
-					return module.showProgressDialog("loading_msg", originalButtons)
-				})
+		// If the promise is pending and does not resolve in 100ms, show progress dialog
+		if (originalButtons.isPending()) {
+			buttons = Promise.race([
+				originalButtons,
+					Promise.all([
+						Promise.delay(100),
+						asyncImport(typeof module !== "undefined" ? module.id : __moduleName,
+							`${env.rootPathPrefix}src/gui/base/ProgressDialog.js`)
+					]).then(([_, module]) => {
+						if (originalButtons.isPending()) {
+							return module.showProgressDialog("loading_msg", originalButtons)
+						} else {
+							return originalButtons
+						}
+					})
+				]
+			)
 		}
 		buttons.then(buttons => {
 			let dropdown = new DropdownN(() => buttons, width)

src/mail/MailEditor.js
@@ -1157,22 +1157,24 @@ export class MailEditor {
 				})
 				return
 			}
-			const editor = new MailEditor(mailModel.getUserMailboxDetails())
-			let signature = "<br><br>--"
-			signature += "<br>Client: " + client.getIdentifier()
-			signature += "<br>Tutanota version: " + env.versionNumber
-			signature += "<br>Time zone: " + getTimeZone()
-			signature += "<br>User agent:<br>" + navigator.userAgent
-			editor.initWithTemplate({to: [{name: null, address: "premium@tutao.de"}]}, "", signature, true).then(() => {
-				editor.show()
+			return mailModel.getUserMailboxDetails().then((mailboxDetails) => {
+				const editor = new MailEditor(mailboxDetails)
+				let signature = "<br><br>--"
+				signature += "<br>Client: " + client.getIdentifier()
+				signature += "<br>Tutanota version: " + env.versionNumber
+				signature += "<br>Time zone: " + getTimeZone()
+				signature += "<br>User agent:<br>" + navigator.userAgent
+				editor.initWithTemplate({to: [{name: null, address: "premium@tutao.de"}]}, "", signature, true).then(() => {
+					editor.show()
+				})
 			})
 		})
 
 	}
 
 	static writeInviteMail() {
-		mailModel.init().then(() => {
-			const editor = new MailEditor(mailModel.getUserMailboxDetails())
+		mailModel.getUserMailboxDetails().then((mailboxDetails) => {
+			const editor = new MailEditor(mailboxDetails)
 			const username = logins.getUserController().userGroupInfo.name;
 			const body = lang.get("invitationMailBody_msg", {
 				'{registrationLink}': "https://mail.tutanota.com/signup",

src/mail/MailListView.js
@@ -95,10 +95,12 @@ export class MailListView implements Component {
 						return Promise.resolve() // externals don't have an archive folder
 					} else if (this.targetInbox()) {
 						this.list.selectNone()
-						return mailModel.moveMails([listElement], getInboxFolder(mailModel.getMailboxFolders(listElement)))
+						return mailModel.getMailboxFolders(listElement)
+						                .then((folders) => mailModel.moveMails([listElement], getInboxFolder(folders)))
 					} else {
 						this.list.selectNone()
-						return mailModel.moveMails([listElement], getArchiveFolder(mailModel.getMailboxFolders(listElement)))
+						return mailModel.getMailboxFolders(listElement)
+						                .then((folders) => mailModel.moveMails([listElement], getArchiveFolder(folders)))
 					}
 				},
 				enabled: true
@@ -129,13 +131,16 @@ export class MailListView implements Component {
 		}, 0)
 		const counterValue = mailModel.getCounterValue(this.listId)
 		if (counterValue != null && counterValue !== unreadMails) {
-			const data = createWriteCounterData({
-				counterType: CounterType_UnreadMails,
-				row: mailModel.getMailboxDetailsForMailListId(this.listId).mailGroup._id,
-				column: this.listId,
-				value: String(unreadMails)
+			mailModel.getMailboxDetailsForMailListId(this.listId).then((mailboxDetails) => {
+				const data = createWriteCounterData({
+					counterType: CounterType_UnreadMails,
+					row: mailboxDetails.mailGroup._id,
+					column: this.listId,
+					value: String(unreadMails)
+				})
+				serviceRequestVoid(MonitorService.CounterService, HttpMethod.POST, data)
 			})
-			serviceRequestVoid(MonitorService.CounterService, HttpMethod.POST, data)
+
 		}
 	})
 
@@ -191,24 +196,25 @@ export class MailListView implements Component {
 
 	_loadMailRange(start: Id, count: number): Promise<Mail[]> {
 		return loadRange(MailTypeRef, this.listId, start, count, true).then(mails => {
-			let mailboxDetail = mailModel.getMailboxDetailsForMailListId(this.listId)
-			if (isInboxList(mailboxDetail, this.listId)) {
-				// filter emails
-				return Promise.filter(mails, (mail) => {
-					return findAndApplyMatchingRule(mailboxDetail, mail).then(matchingMailId => !matchingMailId)
-				}).then(inboxMails => {
-					if (mails.length === count && inboxMails.length < mails.length) {
-						//console.log("load more because of matching inbox rules")
-						return this._loadMailRange(mails[mails.length - 1]._id[1], mails.length - inboxMails.length)
-						           .then(filteredMails => {
-							           return inboxMails.concat(filteredMails)
-						           })
-					}
-					return inboxMails
-				})
-			} else {
-				return mails
-			}
+			return mailModel.getMailboxDetailsForMailListId(this.listId).then((mailboxDetail) => {
+				if (isInboxList(mailboxDetail, this.listId)) {
+					// filter emails
+					return Promise.filter(mails, (mail) => {
+						return findAndApplyMatchingRule(mailboxDetail, mail).then(matchingMailId => !matchingMailId)
+					}).then(inboxMails => {
+						if (mails.length === count && inboxMails.length < mails.length) {
+							//console.log("load more because of matching inbox rules")
+							return this._loadMailRange(mails[mails.length - 1]._id[1], mails.length - inboxMails.length)
+							           .then(filteredMails => {
+								           return inboxMails.concat(filteredMails)
+							           })
+						}
+						return inboxMails
+					})
+				} else {
+					return mails
+				}
+			})
 		})
 	}
 }

src/mail/MailModel.js
@@ -1,7 +1,7 @@
 //@flow
 import m from "mithril"
 import stream from "mithril/stream/stream.js"
-import {containsEventOfType, neverNull, noOp} from "../api/common/utils/Utils"
+import {containsEventOfType, defer, identity, neverNull, noOp} from "../api/common/utils/Utils"
 import {createMoveMailData} from "../api/entities/tutanota/MoveMailData"
 import {load, loadAll, serviceRequestVoid} from "../api/main/Entity"
 import {TutanotaService} from "../api/entities/tutanota/Services"
@@ -45,6 +45,7 @@ export type MailboxCounters = {
 }
 
 export class MailModel {
+	/** Empty stream until init() is finished, exposed mostly for map()-ing, use getMailboxDetails to get a promise */
 	mailboxDetails: Stream<MailboxDetail[]>
 	mailboxCounters: Stream<MailboxCounters>
 	_initialization: ?Promise<void>
@@ -52,7 +53,7 @@ export class MailModel {
 	_eventController: EventController
 
 	constructor(notifications: Notifications, eventController: EventController) {
-		this.mailboxDetails = stream([])
+		this.mailboxDetails = stream()
 		this.mailboxCounters = stream({})
 		this._initialization = null
 		this._notifications = notifications
@@ -119,29 +120,48 @@ export class MailModel {
 		})
 	}
 
-	getMailboxDetails(mail: Mail): MailboxDetail {
+	getMailboxDetails(): Promise<Array<MailboxDetail>> {
+		if (this.mailboxDetails()) {
+			return Promise.resolve(this.mailboxDetails())
+		} else {
+			// Convert Stream to Promise: wait until the first value, resovle promise and unsubscribe from stream.
+			const deferred = defer()
+			const dependency = this.mailboxDetails.map(identity)
+			dependency.map((mailboxDetails) => {
+				deferred.resolve(mailboxDetails)
+				dependency.end(true)
+			})
+			return deferred.promise
+		}
+	}
+
+	getMailboxDetailsForMail(mail: Mail): Promise<MailboxDetail> {
 		return this.getMailboxDetailsForMailListId(mail._id[0])
 	}
 
-	getMailboxDetailsForMailListId(mailListId: Id): MailboxDetail {
-		return neverNull(this.mailboxDetails().find((md) => md.folders.find(f => f.mails === mailListId) != null))
+	getMailboxDetailsForMailListId(mailListId: Id): Promise<MailboxDetail> {
+		return this.getMailboxDetails().then(mailboxDetails =>
+			neverNull(mailboxDetails.find((md) => md.folders.find(f => f.mails === mailListId) != null)))
 	}
 
-	getMailboxDetailsForMailGroup(mailGroupId: Id): MailboxDetail {
-		return neverNull(this.mailboxDetails().find((md) => mailGroupId === md.mailGroup._id))
+	getMailboxDetailsForMailGroup(mailGroupId: Id): Promise<MailboxDetail> {
+		return this.getMailboxDetails().then(mailboxDetails =>
+			neverNull(mailboxDetails.find((md) => mailGroupId === md.mailGroup._id)))
 	}
 
-	getUserMailboxDetails(): MailboxDetail {
+	getUserMailboxDetails(): Promise<MailboxDetail> {
 		let userMailGroupMembership = logins.getUserController().getUserMailGroupMembership()
-		return neverNull(this.mailboxDetails().find(md => md.mailGroup._id === userMailGroupMembership.group))
+		return this.getMailboxDetails().then(mailboxDetails =>
+			neverNull(mailboxDetails.find((md) => md.mailGroup._id === userMailGroupMembership.group)))
 	}
 
-	getMailboxFolders(mail: Mail): MailFolder[] {
-		return this.getMailboxDetails(mail).folders
+	getMailboxFolders(mail: Mail): Promise<MailFolder[]> {
+		return this.getMailboxDetailsForMail(mail).then(md => md.folders)
 	}
 
 	getMailFolder(mailListId: Id): ?MailFolder {
-		for (let e of this.mailboxDetails()) {
+		const mailboxDetails = this.mailboxDetails() || []
+		for (let e of mailboxDetails) {
 			for (let f of e.folders) {
 				if (f.mails === mailListId) {
 					return f
@@ -193,7 +213,7 @@ export class MailModel {
 		}
 		if (mailBuckets.move.size > 0) {
 			for (const [folderId, mails] of mailBuckets.move) {
-				promises.push(mailModel.moveMails(mails, getTrashFolder(mailModel.getMailboxFolders(mails[0]))))
+				promises.push(mailModel.getMailboxFolders(mails[0]).then(folders => mailModel.moveMails(mails, getTrashFolder(folders))))
 			}
 		}
 		return Promise.all(promises).return()
@@ -212,10 +232,11 @@ export class MailModel {
 					load(UserTypeRef, update.instanceId).then(updatedUser => {
 						let newMemberships = updatedUser.memberships
 						                                .filter(membership => membership.groupType === GroupType.Mail)
-						let currentDetails = this.mailboxDetails()
-						if (newMemberships.length !== currentDetails.length) {
-							this._init().then(() => m.redraw())
-						}
+						this.getMailboxDetails().then(mailboxDetails => {
+							if (newMemberships.length !== mailboxDetails.length) {
+								this._init().then(() => m.redraw())
+							}
+						})
 					})
 				}
 			} else if (isUpdateForTypeRef(MailTypeRef, update) && update.operation === OperationType.CREATE) {
@@ -223,13 +244,11 @@ export class MailModel {
 				if (folder && folder.folderType === MailFolderType.INBOX
 					&& !containsEventOfType(updates, OperationType.DELETE, update.instanceId)) {
 					// If we don't find another delete operation on this email in the batch, then it should be a create operation
-					const mailboxDetail = mailModel.getMailboxDetailsForMailListId(update.instanceListId)
 					const mailId = [update.instanceListId, update.instanceId]
 					load(MailTypeRef, mailId)
-						.then(mail => {
-							return findAndApplyMatchingRule(mailboxDetail, mail)
-								.then((newId) => this._showNotification(newId || mailId))
-						})
+						.then((mail) => mailModel.getMailboxDetailsForMailListId(update.instanceListId)
+						                         .then(mailboxDetail => findAndApplyMatchingRule(mailboxDetail, mail))
+						                         .then((newId) => this._showNotification(newId || mailId)))
 						.catch(noOp)
 				}
 			}
@@ -256,8 +275,8 @@ export class MailModel {
 	getCounterValue(listId: Id): ?number {
 		const mailboxDetails = this.getMailboxDetailsForMailListId(listId)
 		const counters = this.mailboxCounters()
-		if (mailboxDetails && counters[mailboxDetails.mailGroup._id]) {
-			return counters[mailboxDetails.mailGroup._id][listId]
+		if (mailboxDetails.isFulfilled() && counters[mailboxDetails.value().mailGroup._id]) {
+			return counters[mailboxDetails.value().mailGroup._id][listId]
 		} else {
 			return null
 		}

commit 73698d4a252bedcbc3890d74a39750764e063161

Do not expect search filters to exist too early, fix #1790
Regression after 152846866da7c6e0d849ae454d86939c034075a8

We changed mailboxDetails stream to not have default value until it's
loaded and we don't create search view filters until it's initialized.

src/search/SearchView.js
@@ -73,8 +73,8 @@ export class SearchView implements CurrentView {
 	_time: TextField;
 	_endDate: ?Date; // null = today
 	_startDate: ?Date; // null = current mail index date. this allows us to start the search (and the url) without end date set
-	_mailFolderSelection: DropDownSelector<?string>;
-	_mailFieldSelection: DropDownSelector<?string>;
+	_mailFolderSelection: ?DropDownSelector<?string>;
+	_mailFieldSelection: ?DropDownSelector<?string>;
 
 	_doNotUpdateQuery: boolean;
 
@@ -128,7 +128,7 @@ export class SearchView implements CurrentView {
 		this._mailFieldSelection.selectedValue.map(newValue => {
 			if (logins.getUserController().isFreeAccount()) {
 				if (newValue != null) {
-					this._mailFieldSelection.selectedValue(null)
+					neverNull(this._mailFieldSelection).selectedValue(null)
 					showNotAvailableForFreeDialog(true)
 				}
 			} else {
@@ -162,7 +162,7 @@ export class SearchView implements CurrentView {
 			this._mailFolderSelection.selectedValue.map(newValue => {
 				if (logins.getUserController().isFreeAccount()) {
 					if (newValue != null) {
-						this._mailFolderSelection.selectedValue(null)
+						neverNull(this._mailFolderSelection).selectedValue(null)
 						showNotAvailableForFreeDialog(true)
 					}
 				} else {
@@ -215,8 +215,8 @@ export class SearchView implements CurrentView {
 								]),
 								m(".plr-l.mt-negative-s", [
 									m(this._getUpdatedTimeField()),
-									m(this._mailFieldSelection),
-									m(this._mailFolderSelection),
+									this._mailFieldSelection ? m(this._mailFieldSelection) : null,
+									this._mailFolderSelection ? m(this._mailFolderSelection) : null,
 								])
 							])
 							: null
@@ -358,8 +358,8 @@ export class SearchView implements CurrentView {
 			searchCategory,
 			(this._endDate) ? getEndOfDay(this._endDate).getTime() : null,
 			(this._startDate) ? getStartOfDay(this._startDate).getTime() : null,
-			this._mailFieldSelection.selectedValue(),
-			this._mailFolderSelection.selectedValue()
+			this._mailFieldSelection && this._mailFieldSelection.selectedValue(),
+			this._mailFolderSelection && this._mailFolderSelection.selectedValue()
 		)
 		return getSearchUrl(locator.search.lastQuery(), restriction, selectedId)
 	}
@@ -494,8 +494,8 @@ export class SearchView implements CurrentView {
 			this._doNotUpdateQuery = true
 			this._endDate = restriction.start ? new Date(restriction.start) : null
 			this._startDate = restriction.end ? new Date(restriction.end) : null
-			this._mailFolderSelection.selectedValue(restriction.listId)
-			this._mailFieldSelection.selectedValue(restriction.field)
+			this._mailFolderSelection && this._mailFolderSelection.selectedValue(restriction.listId)
+			this._mailFieldSelection && this._mailFieldSelection.selectedValue(restriction.field)
 			this._doNotUpdateQuery = false
 		}
 